68a05d1f735ec6aa88adb779d0561dba914e6fb3
furkangergin <hunter_1325@hotmail.com>
2022-10-17 11:40:40 +0300

    commit8

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"
-        //publish = false
+        tags = "@wip",
+        publish = false
 
 )
 public class CukesRunner {
